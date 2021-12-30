de7743b755eabf61ed53519f38417ad407960462
Valerio Giocondi <valerio.giocondi@gmail.com>
2021-12-30 09:01:06 +0100

    FE - MessageQueue / PubSubJS abstraction

---
+++ front-end/src/services/messageQueue.service.ts
@@ -0,0 +1,25 @@
+// UTILS
+import PubSub from 'pubsub-js'
+
+
+class MessageQueue  { 
+
+    subscribe(topic: string, cb: any): void {
+        
+        PubSub.subscribe(topic, cb);
+    }
+
+    publish(topic: string, cb: any): void {
+        
+        PubSub.publish(topic, cb);
+    }
+
+    unsubscribe(topic: string): void {
+        
+        PubSub.unsubscribe(topic);
+    }
+}
+
+// SINGLETON
+// Export an instance of the class directly
+export default new MessageQueue();
